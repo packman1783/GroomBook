f7d9bb01826b1bf9ac3e6d890db1945143e7a0ae
Denis Saplitsyn <d.saplitsyn@mail.astondevs.ru>
2026-05-27 20:43:11 +0300

    initial commit

---
+++ src/test/java/org/example/groombook/GroomBookApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.groombook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroomBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
