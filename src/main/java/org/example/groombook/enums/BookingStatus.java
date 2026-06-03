d67687c64716074ac4f0ec90cfc8a034c7d9dc4a
Denis Saplitsyn <d.saplitsyn@mail.astondevs.ru>
2026-06-03 16:34:15 +0300

    создал enum BookingStatus

---
+++ src/main/java/org/example/groombook/enums/BookingStatus.java
@@ -0,0 +1,10 @@
+package org.example.groombook.enums;
+
+public enum BookingStatus {
+    PENDING,
+    CONFIRMED,
+    CANCELLED_BY_CLIENT,
+    CANCELLED_BY_MASTER,
+    COMPLETED,
+    NO_SHOW
+}
