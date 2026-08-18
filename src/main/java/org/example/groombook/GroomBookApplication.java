5243edd03a4bca646261c6bf95acdbf0d77eb002
Denis Saplitsyn <d.saplitsyn@mail.astondevs.ru>
2026-08-18 21:15:56 +0300

    добавил видимость .env при запуске приложения

--- src/main/java/org/example/groombook/GroomBookApplication.java
+++ src/main/java/org/example/groombook/GroomBookApplication.java
@@ -4,11 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @EnableScheduling
 public class GroomBookApplication {
 
     public static void main(String[] args) {
+        // systemProperties() делает переменные из .env видимыми для @Value("${...}")
+        Dotenv.configure().systemProperties().load();
+
         SpringApplication.run(GroomBookApplication.class, args);
     }
 
