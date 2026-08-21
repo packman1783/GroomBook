8020e461ca790d69a1cf6c81daa70d88e9ea4d21
Denis Saplitsyn <d.saplitsyn@mail.astondevs.ru>
2026-08-21 17:26:13 +0300

    Обновлено исключение при сообщении об ошибке при превышении лимита записей на неделю

--- src/main/java/org/example/groombook/exception/BookingLimitExceededException.java
+++ src/main/java/org/example/groombook/exception/BookingLimitExceededException.java
@@ -1,12 +1,21 @@
 package org.example.groombook.exception;
 
+import java.time.LocalDate;
+
 /**
  * Клиент исчерпал лимит броней на текущую неделю (максимум 2).
  * Бот: "На этой неделе вы уже записаны максимальное количество раз."
  */
 public class BookingLimitExceededException extends GroomBookException {
 
-    public BookingLimitExceededException(Long clientId) {
-        super("Клиент #" + clientId + " превысил лимит броней на неделю");
+    private final LocalDate nextAvailableDate;
+
+    public BookingLimitExceededException(Long clientId, LocalDate nextAvailableDate) {
+        super("Клиент #" + clientId + " превысил лимит броней на неделю. Следующая запись возможна с " + nextAvailableDate);
+        this.nextAvailableDate = nextAvailableDate;
+    }
+
+    public LocalDate getNextAvailableDate() {
+        return nextAvailableDate;
     }
 }
